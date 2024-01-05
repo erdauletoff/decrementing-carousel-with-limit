f0ef075a3b80cee00e09a0789e9c39799a2f66ff
Darkhan <darkhan_erdawletov@student.itpu.uz>
2024-01-05 12:15:27 +0500

    done

--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -1,7 +1,35 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel{
+    private final int actionLimit;
+    static int[] carouselArray;
+    private int carouselRunCounter;
+    private boolean isCarouselRunning;
+
+
     public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
         super(capacity);
+        this.actionLimit = actionLimit;
+        carouselArray = new int[capacity];
+    }
+
+    @Override
+    public boolean addElement(int element){
+        if (counter < capacity && element > 0 && !isCarouselRunning) {
+            carouselArray[counter] = element;
+            counter++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public CarouselRun run(){
+        if (carouselRunCounter == 0) {
+            isCarouselRunning = true;
+            carouselRunCounter++;
+            return new LimitedCarouselRun(actionLimit);
+        }
+        return null;
     }
 }
